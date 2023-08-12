eb85a0d611ed22f2ad439dbcb42b229fbb8dcf4e
Alex da Silva <1806596@aluno.univesp.br>
2023-08-12 00:17:02 -0300

    Adicionado tratamento de exceções

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,8 +6,16 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Por favor, digite o número da Conta: ");
-        int numeroConta = scanner.nextInt();
+        int numeroConta;
+        while (true)
+            try {
+                System.out.println("Por favor, digite o número da Conta: ");
+                numeroConta = Integer.parseInt(scanner.nextLine());
+                break;
+            }
+            catch (NumberFormatException nfe){
+                System.out.println("Número da conta deve conter apenas números");
+            }
 
         System.out.println("Por favor, digite o número da Agência: ");
         String agencia = scanner.next();
@@ -16,9 +25,18 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o seu nome: ");
         String nomeCliente = scanner.nextLine();
 
-        System.out.println("Por favor, digite o seu saldo: ");
-        double saldo = scanner.nextDouble();
+        double saldo;
+        while (true)
+            try {
+                System.out.println("Por favor, digite o seu saldo: ");
+                saldo = Double.parseDouble(scanner.nextLine());
+                break;
+            }
+            catch (NumberFormatException nfe){
+                System.out.println("Número da conta deve conter apenas números");
+            }
 
         System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        scanner.close();
     }
 }
